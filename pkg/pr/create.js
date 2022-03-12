342053af1ded0aecd58ffa9fdc7cdd380bb4866e
choshsh <cho911115@gmail.com>
2022-03-12 18:01:10 +0900

    sad

--- pkg/pr/create.js
+++ pkg/pr/create.js
@@ -1,4 +1,4 @@
-module.exports = async ({ github, context, exec }) => {
+module.exports = async ({ github, context, exec, inputs }) => {
   // github.rest.pulls.create({
   //   title: context.payload.head_commit.message,
   //   owner: context.repo.owner,
@@ -8,6 +8,7 @@ module.exports = async ({ github, context, exec }) => {
   //   body: "Created by DevOps bot.",
   // });
   console.log(context);
+  console.log(inputs);
   const head = "${{ inputs.head }}";
   const base = "${{ inputs.base }}";
   const title = context.payload.head_commit.message;
