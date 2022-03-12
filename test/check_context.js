848f17a0debf520a7e4de458718f1a94f5f773a3
choshsh <cho911115@gmail.com>
2022-03-12 14:21:30 +0900

    edit

--- test/check_context.js
+++ test/check_context.js
@@ -1,5 +1,7 @@
-module.exports = async ({ github, context, core }) => {
+module.exports = ({ github, context, core }) => {
   const { SHA } = process.env;
+
+  console.log(`Print env & context\n`);
   console.log(`env : ${SHA}`);
   console.log(`context : ${context}`);
   console.log(`github : ${github}`);
